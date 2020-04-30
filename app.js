38a2e924ed39834e3654b58b7bf3439966bfc056
Romario Jashari <38104868+romariojashari@users.noreply.github.com>
2020-04-29 20:18:53 -0400

    Added Amortization Table

--- app.js
+++ app.js
@@ -1,5 +1,3 @@
-// Listen for submit
-
 document.getElementById("input-loan").addEventListener("submit", calculate);
 
 function calculate(e) {
@@ -13,7 +11,6 @@ function calculate(e) {
     const purchasePrice = parseFloat(cost);
     const downPayment = parseFloat(down);
     const p = purchasePrice - downPayment;
-    console.log(p);
     const r = parseFloat(interest) / 100 / 12; // Converting to % and monthly.
     const n = parseFloat(term) * 12; // Convert years to months.
 
@@ -22,7 +19,7 @@ function calculate(e) {
     const monthlyPayment = pmt.toFixed(2); // 2 decimal digits. $xxx.xx
 
     //Compute Total Payment
-    const totalPayment = pmt * n;
+    const totalPayment = pmt * n; // Total payback, includes interest.
 
     // Show Results
     document.getElementById("monthlyPayment").innerHTML = "$" + monthlyPayment.toLocaleString();
@@ -31,6 +28,30 @@ function calculate(e) {
         minimumFractionDigits: 2,
         maximumFractionDigits: 2
     });
+
+    // Adds row to the body of the table. To be continued...
+    // var newRow=document.getElementById('tbody').insertRow();
+    // newRow.innerHTML = "<td>1</td><td>2</td><td>3</td><td>4</td><td>5</td><td>6</td>";
+
+    var currentBalance = p; // p is the principle which is the amount of money borrowed. 
+    var paymentCounter = 1;
+    var totalInterest = 0;
+    var towardsEquity = downPayment;
+
+    while(currentBalance > 0) {
+        towardsInterest = r*currentBalance; // Calculates interest in monthly payment.
+        towardsPrinciple = pmt /*monthly payment*/ - towardsInterest;
+        currentBalance -= towardsPrinciple;
+        towardsEquity += towardsPrinciple;
+        percentEquity = towardsEquity/purchasePrice*100;
+
+        // Display row
+        var newRow=document.getElementById('tbody').insertRow();
+        newRow.innerHTML = "<td>"+paymentCounter+"</td><td>$"+currentBalance.toFixed(2)+"</td><td>$"+monthlyPayment+"</td><td>$"+towardsInterest.toFixed(2)+"</td><td>$"+towardsPrinciple.toFixed(2)+"</td><td>"+towardsEquity.toFixed(2)+"</td><td>"+percentEquity.toFixed(2)+" %</td>";
+
+        paymentCounter++;
+
+    }  
 
     e.preventDefault();
 }
